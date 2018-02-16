8fc731e560ff9c4301986414377f3c437f865b93
DESKTOP-34CHPOI\Willie Smith <willie.smith3@hotmail.com>
2018-02-16 18:23:32 -0500

    added pagination to alert-panel

--- app/src/components/alert-panel.js
+++ app/src/components/alert-panel.js
@@ -6,15 +6,28 @@ export default class AlertPanel extends Component {
 		super(props)
     this.state = {
       alertList: [],
-      message: this.props.message
+      message: this.props.message,
+      page: 1,
+      pageLoc: 50
     }
 		this.showAlerts = this.showAlerts.bind(this)
+    this.pagination = this.pagination.bind(this)
 	}
+  componentWillMount(){
+    this.pagination(1)
+  }
 	componentWillReceiveProps(nextProps){
     if(this.props.message !== nextProps.message){
       this.setState({message:nextProps.message})
     }
   }
+  // shouldComponentUpdate(nextProps, nextState){
+  //   if(this.props.alerts !== nextProps.alerts){
+  //     return true
+  //   }elseelse{
+  //     return false
+  //   }
+  // }
 	showAlerts(){
 		if(this.props.alerts){
 			let listAlerts = this.props.alerts.map((a, i)=>{
@@ -24,8 +37,17 @@ export default class AlertPanel extends Component {
 		return listAlerts
     }
 	}
+  pagination(page){
+    console.log('clicked the page')
+    let pageLoc = page * 50;
+    let alerts = this.showAlerts()
+    let pagination = alerts.slice((pageLoc - 50), pageLoc)
+    console.log(pagination)
+    this.setState({alertList:pagination,page,pageLoc},()=>{console.log(this.state.page)})
+  }
   render(){
-    let alertList = this.showAlerts()
+    console.log('called render in alert-panel')
+    let alertList = this.state.alertList
     let size = this.props.size
     let message = this.props.message
     let showAlerts = this.props.alerts && this.props.alerts.length < 1 ? message : alertList
@@ -44,6 +66,26 @@ export default class AlertPanel extends Component {
         <div className="alert-panel-alerts-container">
 					{showAlerts}
         </div>
+        {this.props.alerts.length > 50 ?
+          <div className="alert-panel-bottom">
+            <button
+              className="alert-panel-pagination-previous"
+              onClick={this.pagination.bind(this,this.state.page - 1)}>
+              {"Previous"}
+            </button>
+            <button 
+              className="alert-panel-pagination">
+              {this.state.pageLoc - 49 + " to " + this.state.pageLoc} 
+            </button>
+            <button 
+              className="alert-panel-pagination-next"
+              onClick={this.pagination.bind(this,this.state.page + 1)}>
+              {"Next"}
+            </button>
+          </div>
+          :
+          null
+        }  
       </div>
     )
   }
